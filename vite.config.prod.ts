902a6c867f26adb33024e3afad2e7f176aea9ed6
richardevcom <hello@richardev.com>
2024-07-26 04:41:28 +0300

    ♻️ Refactored

--- vite.config.prod.ts
+++ vite.config.prod.ts
@@ -2,11 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   logLevel: 'silent',
-  plugins: [vue()],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
