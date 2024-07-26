53582e89c1d427037da1a18a1e74d2d587373edc
richardevcom <hello@richardev.com>
2024-07-26 04:07:38 +0300

    ♻️ Refactored

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp } from "vue"
+import { createPinia } from "pinia"
 
-import App from "./App.vue";
-import router from "./router";
+import App from "./App.vue"
+import router from "./router"
 
-import "./assets/base.scss";
+import "./assets/base.scss"
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.use(createPinia());
-app.use(router);
-
-app.mount("#app");
+app.use(createPinia()).use(router).mount("#app")
